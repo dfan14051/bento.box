1fe62ba466b14f0cf03b3fde01f9f551f5d6d7c8
Hun Jae Lee <hunjael@gmail.com>
2017-06-16 16:19:53 -0400

    :palm_tree: added orders and comments

---
+++ app/views/comments/_comment.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! comment, :id, :order_id, :content, :User, :created_at, :updated_at
+json.url comment_url(comment, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :orders
   devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'orders#index', as: :authenticated_root
+    end
+      root :to => 'welcome#index', as: :unauthenticated_root
+  end
+
   root 'welcome#index'
 end
